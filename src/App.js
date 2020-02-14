876d51e5a140d677bf5b59b3f6a6fb81f9e14e5e
Nishant k <nishant.kumar@neosofttech.com>
2020-02-14 18:46:32 +0530

    switch handler functionlity implementation

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import Person from './Person/Person';
 
 class App extends Component
 {
+    state={
+        persons:[
+            {name:'Sagar' ,age:24},
+            {name:'Vishanl', age:25},
+            {name:'Rahul', age:28}
+        ]
+    }
+    switchNameHandler=()=>
+    {
+        console.log('Clicked')
+        this.state.persons[0].name='Jack';
+    }
   render ()
   {
     return (
         <div className="App">
-        <Person name="Nishank" age="26"/>
-        <Person name="Sagar" age="25"/>
+            <Person name="Prashant" age="27">CI Developer </Person>
+            <button onClick={this.switchNameHandler}>Switch Button</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
         </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-class Person extends React.Component{
+/*class Person extends Component{
     render ()
     {
         return (
            <p>I'm a Person {props.name} and i'm {props.age} year old.</p>
         );
     }
+}
+
+ */
+
+const Person = (props) =>
+
+{
+    return <p>I'm a Person {props.name } and i'm {props.age} year old.</p>
 }
 export default Person;
