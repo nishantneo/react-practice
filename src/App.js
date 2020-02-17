d17b50901183b16b7ff77dbd5bd0b2bc92e05574
Nishant k <nishant.kumar@neosofttech.com>
2020-02-17 11:44:21 +0530

    Two way binding commit

--- src/App.js
+++ src/App.js
@@ -12,18 +12,35 @@ class App extends Component
             {name:'Rahul', age:28}
         ]
     }
-    switchNameHandler=()=>
+    switchNameHandler=(newName,newAge)=>
     {
         console.log('Clicked')
+        this.setState({
+             persons :[
+                 {name:newName , age:newAge},
+                 {name:'martin',age:24},
+                 {name:'john', age:25}
+             ]
+            })
         this.state.persons[0].name='Jack';
+    }
+    nameChangedHandler=(event)=>
+    {
+        this.setState({
+        persons:[
+            {name:'johny',age:34},
+            {name:'mark',age:23},
+            {name:'jack',age:24}
+        ]
+        })
     }
   render ()
   {
     return (
         <div className="App">
             <Person name="Prashant" age="27">CI Developer </Person>
-            <button onClick={this.switchNameHandler}>Switch Button</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <button onClick={this.switchNameHandler.bind(this,'martin').bind(this,70)}>Switch Button</button>
+            <Person changed={this.nameChangedHandler} click={this.switchNameHandler.bind(this,'evon').bind(this,50)} name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -14,6 +14,13 @@ import React,{Component} from 'react';
 const Person = (props) =>
 
 {
-    return <p>I'm a Person {props.name } and i'm {props.age} year old.</p>
+    return (
+            <div>
+                <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
+                <p>{props.children}</p>
+                <input type='text' onChange={props.changed} value={props.name}/>
+            </div>
+        )
+
 }
 export default Person;
